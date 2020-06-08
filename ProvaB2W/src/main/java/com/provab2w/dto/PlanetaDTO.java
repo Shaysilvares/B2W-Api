4624d1bbd8bea9f3b23a965c73e2102f3efed462
Shayna Bauer <shaynasilvares2@hotmail.com>
2020-06-07 21:59:25 -0300

    Correção do JsonIgnore e add Junit 4 ao pom

--- ProvaB2W/src/main/java/com/provab2w/dto/PlanetaDTO.java
+++ ProvaB2W/src/main/java/com/provab2w/dto/PlanetaDTO.java
@@ -2,6 +2,8 @@ package com.provab2w.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PlanetaDTO {
 
